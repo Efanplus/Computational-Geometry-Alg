183ecf2cbffc52a94abee292cdc0d2ef231c0846
Efanplus <yfwbh@outlook.com>
2018-10-18 11:47:22 +0800

    Toys

---
+++ POJ2318_Toys.cpp
@@ -0,0 +1,85 @@
+﻿#include "stdafx.h"
+#include "POJ2318_Toys.h"
+#include <unordered_map>
+
+
+POJ2318_Toys::POJ2318_Toys()
+{
+
+}
+
+
+POJ2318_Toys::~POJ2318_Toys()
+{
+}
+
+bool POJ2318_Toys::in()
+{
+	cin >> n;
+	if (n == 0)return false;
+	pointer L, R;
+	cin >> m >> L.x >> L.y >> R.x >> R.y;
+	LU.push_back(L);
+	RD.push_back(R);
+	vector<pointer> toys;
+	vector<line> pors;
+//	porlines.clear();
+	//toypointers.clear();
+	for (int i = 0; i < n; i++) {
+		pointer start, end;
+		cin >> start.x >> end.x;
+		start.y = L.y;
+		end.y = R.y;
+		line l = line(start, end);
+		pors.push_back(l);
+	}
+	for (int j = 0; j < m; j++) {
+		pointer p;
+		cin >> p.x >> p.y;
+		toys.push_back(p);
+	}
+	porlines.push_back(pors);
+	toypointers.push_back(toys);
+	return true;
+}
+
+//返回的是toy是否在l的左侧
+bool POJ2318_Toys::left(line l, pointer p)
+{
+	//利用C++的短路效应
+	return p.x < l.xmin || (p.x < l.xmax && (l.en.x - l.st.x)*(p.y - l.st.y) - (p.x - l.st.x)*( l.en.y - l.st.y) < 0);
+}
+
+//返回的是toy所在的分区号
+int POJ2318_Toys::judge(pointer p, vector<line> porlines)
+{
+//	int res = 0;
+//	while (res < porlines.size() && !left(porlines[res], p))res++;
+	if (!left(porlines.back(), p)) return porlines.size();
+	int l = 0, r = porlines.size() - 1;
+	while (l <= r) {
+		int suanz = (l + r) / 2;
+		if (!left(porlines[suanz], p))l = suanz + 1;
+		else r = suanz - 1;
+	}
+	return l;
+}
+
+void POJ2318_Toys::doing()
+{
+	cout << "the result is:" << endl;
+	for (int i = 0; i < toypointers.size(); i ++) {
+		vector<pointer> toys = toypointers[i];
+		vector<line> pors = porlines[i];
+		unordered_map<int, int>m;
+		for (int i = 0; i < toys.size(); i++) {
+			m[judge(toys[i], pors)] ++;
+		}
+		for (int i = 0; i <= pors.size(); i++)
+			cout << i << ": " << m[i] << endl;
+		if (!toypointers.empty())cout << endl;
+	}
+}
+
+
+
---
+++ POJ2318_Toys.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+class pointer{
+public:
+	int x;
+	int y;
+	pointer() : x(0), y(0) {}
+	pointer(int a, int b) : x(a),  y(b)  {}
+	pointer& operator =(const pointer& p) {
+		x = p.x;
+		y = p.y;
+		return *this;
+	}
+};
+class line {
+public:
+	pointer st;
+	pointer en;
+	int xmin;
+	int xmax;
+	line() {
+		st = pointer();
+		en = pointer();
+		xmin = 0;
+		xmax = 0;
+	}
+	line(pointer start, pointer end) : st(start), en(end), xmin(std::min(start.x, end.x)), xmax(std::max(start.x, end.x)) {}
+};
+class POJ2318_Toys
+{
+public:
+	POJ2318_Toys();
+	~POJ2318_Toys();
+	int n, m;
+	vector<pointer> LU, RD;
+	//对于这种在类里面的头文件中定义的vector，若想声明vector的大小，不能在头文件中声明，可以现在头文件中声明，然后去构造函数中用resize()指明情况，
+	vector<vector<line>>  porlines;
+	vector<vector<pointer>> toypointers;
+	bool in();
+	bool left(line l,pointer p);
+	int judge(pointer p, vector<line> porlines);
+	void doing();
+};
+
